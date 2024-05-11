94bacb556f907f7933ef0b309d3825e9e0c90e99
Statute8234 <danieltower101501@gmail.com>
2024-05-11 19:56:59 -0400

    Done!

--- Output_Exercises.cpp
+++ Output_Exercises.cpp
@@ -8,45 +8,35 @@
 //Write a program that outputs your name and your favorite color.
 //Use the operator endl to create a new line.
 
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
+#include <iostream>
+int main() {
+    std::count << "My name is Statute8234" << std::endl
+    std::count << "My favorite color is Blue.";
+    return 0;
+}
 
 
 //Exercise 2:
 //Write a program that outputs a paragraph.
 //use endl to format the paragraph into 4 lines.
 
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
+#include <iostream>
+int main() {
+    std::count << "This is using c++ to officially practice using the language." << std::endl
+    std::count << "Based on the languages I have used, c++ looks like C coding language." << std::endl
+    std::count << "However, C is not as advanced and less basic." << std::endl
+    std::count << "To conclude this, C++ has its own beniffits.";
+    return 0;
+}
 
 //Exercise 3:
 //Write a variation of "Hello World!" Practice the different ways to perform outputs.
 
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
-//code goes here
+#include <iostream>
+int main() {
+    std::count << "Hello World!" << std::endl
+    std::count << "Hello" << std::endl
+    std::count << "World" << std::endl
+    std::count << "World Hello";
+    return 0;
+}
